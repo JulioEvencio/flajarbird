eeac0e3e048a61a7234b1f2e0c5241ec5715e528
JulioEvencio <julioigreja.dev@gmail.com>
2023-07-30 11:35:11 -0300

    feat: added classes generics

---
+++ flajarbird/src/game/util/Mask.java
@@ -0,0 +1,21 @@
+package game.util;
+
+import java.awt.Rectangle;
+
+public class Mask {
+
+	private Rectangle rectangle;
+
+	public Mask(int x, int y, int width, int height) {
+		this.rectangle = new Rectangle(x, y, width, height);
+	}
+
+	public void update(int x, int y, int width, int height) {
+		rectangle = new Rectangle(x, y, width, height);
+	}
+
+	public Rectangle getRectangle() {
+		return rectangle;
+	}
+
+}
